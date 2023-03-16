6fb414b825ea182d49b969b24b7d777c6da483bc
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-03-16 07:01:12 +0530

    More practise on udemy course mod

--- Examples and Theory/stack.py
+++ Examples and Theory/stack.py
@@ -1,3 +1,28 @@
+'''
+For a normal List:
+
+        0 1 2 3 4 5 6
+        A B C D E F G 
+        ^           ^
+        |           |
+    Adding,          Adding,
+    Poping          Poping
+    is O(n)         is O(1)
+
+    
+For a linked list:
+
+        0 1 2 3 4 5 6
+        A B C D E F G 
+        ^           ^
+        |           |
+    Adding          Adding
+    is O(1)         is O(1)                        
+    Poping          Poping
+    is O(1)         is O(n)
+'''
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -17,4 +42,26 @@ class Stack:
         self.length += 1
 
     def pop(self):
-        pass
+        if self.height == 0:
+            return None
+        elif self.height == 1:
+            self.top = None
+            self.height -= 1 
+            return self.top
+        else:
+            temp = self.top
+            self.top = self.top.next
+            temp.next = None
+            self.height -= 1 
+            return temp.value
+        
+
+    def print_stack(self):
+        node = self.top
+        if self.height == 0:
+            print(None)
+            return
+        while node:
+            print(node.value, end=" ")
+            node = node.next
+        print()
