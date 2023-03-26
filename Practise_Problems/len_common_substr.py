259bf6334c839ee3c7e4ae710f44eb3a3fb72ade
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-03-27 04:06:16 +0530

    Added much more efficient ones

--- Practise_Problems/len_common_substr.py
+++ Practise_Problems/len_common_substr.py
@@ -1,7 +1,7 @@
-_ = int(input())
+# n = int(input())
 l1 = list(map(int, input().split(' ')))
 
-_ = int(input())
+# m = int(input())
 l2 = list(map(int, input().split(' ')))
 
 substrings = []
@@ -10,17 +10,28 @@ flag = 0
 
 for p1_idx,p1 in enumerate(l1):
     temp = p1_idx
-    for p2 in l2:
-        if p1_idx < len(l1):
-            if l1[p1_idx] != p2 :
-                if flag == 1:
+    for p2_idx,p2 in enumerate(l2):
+        if p1_idx < len(l1):# to check if we increased p1_idx to unbounded value
+
+            if l1[p1_idx] == p2:
+                substring.append(p2)
+                p1_idx  += 1
+                if p2_idx == len(l2) - 1:# check the only edge case when last element is part of a common substring
+                    print(substring)
                     substrings.append(substring)
                     substring = []
-                    flag = 0
-                    p1_idx = temp
-                continue
             else:
-                substring.append(p2)
-                p1_idx += 1
-                flag = 1
-print(len(max(substrings)))
+                if substring:
+                    print(substring)
+                    substrings.append(substring)
+                    substring = []
+                p1_idx = temp
+
+                # handling another edge case 
+                if l1[p1_idx] == p2:
+                    substring.append(p2)
+                    p1_idx  += 1
+                continue
+    
+print(substrings)
+print(max(map(len , substrings)))
