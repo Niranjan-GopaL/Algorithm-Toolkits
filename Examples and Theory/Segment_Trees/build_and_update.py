5ac64c31bf97176361a2f5a38882d5409fefb496
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-01 16:42:35 +0530

    idkkkkkkk

--- Examples and Theory/Segment_Trees/build_and_update.py
+++ Examples and Theory/Segment_Trees/build_and_update.py
@@ -16,7 +16,7 @@ def build(idx, l, r):
 
     # base case
     if l == r : 
-        segment_tree[idx] = l
+        segment_tree[idx] = inp[l]
         return
     # normal case
     mid = (l+r)//2
@@ -25,17 +25,24 @@ def build(idx, l, r):
 
     # How you want to build the segment tree [RMQ, RSQ], you can do it below  -----\/
     # RMQ
-    if inp[segment_tree[2*idx+1]] < inp[segment_tree[2*idx+2]]:
-        segment_tree[idx]= segment_tree[2*idx+1]
-    else:
-        segment_tree[idx]= segment_tree[2*idx+2]
 
     #Constructs the same tree that sir gave [ look at ST and BIT pptx in Course materials folder in the repo ]
+    # if inp[segment_tree[2*idx+1]] < inp[segment_tree[2*idx+2]]:
+    #     segment_tree[idx]= segment_tree[2*idx+1]
+    # else:
+    #     segment_tree[idx]= segment_tree[2*idx+2]
+
+    # much more easier to store the element instead of the index
+    segment_tree[idx] = min(segment_tree[2*idx+1], segment_tree[2*idx+2])
+
+
+
 
 def query_and_find(idx,a,b,l,r):
 
     # if segtree's interval is COMPLETELY inside given [a,b]
     if l>=a and r<=b:
+        print(segment_tree[idx])
         return segment_tree[idx]
 
     if r<=a or l>=b:
@@ -45,19 +52,25 @@ def query_and_find(idx,a,b,l,r):
     left_ans = query_and_find(2*idx+1,a,b,l,mid)
     right_ans = query_and_find(2*idx+1,a,b,l,mid)
 
-    # if either side returns inf, we can just return the other side
-    if left_ans == inf or right_ans == inf:
-        if left_ans < right_ans:
-            return left_ans 
-        else:
-            return right_ans
-    # if niether side returns inf, and both are proper indexes from seg_tree
-    # then we compare which element in THAT IDX is least in inp array and return accordingly
-    else:
-        if inp[left_ans] < inp[right_ans]:
-            return left_ans
-        else:
-            return right_ans
+    print(left_ans)
+    print(right_ans)
+
+    return min(left_ans, right_ans)
+
+
+    # # if either side returns inf, we can just return the other side
+    # if left_ans == inf or right_ans == inf:
+    #     if left_ans < right_ans:
+    #         return left_ans 
+    #     else:
+    #         return right_ans
+    # # if niether side returns inf, and both are proper indexes from seg_tree
+    # # then we compare which element in THAT IDX is least in inp array and return accordingly
+    # else:
+    #     if inp[left_ans] < inp[right_ans]:
+    #         return left_ans
+    #     else:
+    #         return right_ans
          
 
 inp = [24,3,45,12,7,36,73,87]
@@ -72,5 +85,5 @@ for _ in range(q):
     a,b =list(map(int, input().split()))
     ans.append(query_and_find(0,a,b,0,len(inp)-1))
 
-for each_ in len(ans):
-    print(each_)
+for i in ans:
+    print(i)
