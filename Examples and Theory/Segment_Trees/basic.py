c682f27ba8ec1f418667d49d98dd644107c5c2a4
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-01 14:18:10 +0530

    working segment tree

--- Examples and Theory/Segment_Trees/basic.py
+++ Examples and Theory/Segment_Trees/basic.py
@@ -1,4 +1,12 @@
+'''
+Seg trees are used when we want "range queries" in "log time"
 
+-> Think from leaves. [[1st main point]]
+   that will be the base case
+-> those 3 CONDITIONS are KEYYYY
+'''
+
+from math import inf
 
 
 def build(idx, l, r):
@@ -6,7 +14,7 @@ def build(idx, l, r):
 
     # base case
     if l == r : 
-        segments[idx] = inp[l]
+        segments[idx] = l
         return
     # normal case
     mid = (l+r)//2
@@ -15,11 +23,23 @@ def build(idx, l, r):
 
     # How you want to build the segment tree [RMQ, RSQ], you can do it below
     # RMaxQ
-    segments[idx] = max(segments[2*idx+1], segments[2*idx+2])
+    segments[idx] = min(segments[2*idx+1], segments[2*idx+2])
 
 
 
-inp = []
-segments = []
 
+
+
+inp = [24,3,45,12,7,36,73,87]
+segments = [-inf]*(4*len(inp))
+build(0,0,len(inp)-1)
+print(segments)
+
+
+
+# q = int(input())
+# ans = []
+# for _ in range(q):
+#     l,r =list(map(int, input().split()))
+#     # ans.append()
 
