e35dbb2a04574029d290258c720b59fada2a6314
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 00:56:37 +0530

    VALID BST finished

--- Practise_Problems/Module_4/Valid_BST.cpp
+++ Practise_Problems/Module_4/Valid_BST.cpp
@@ -26,5 +26,39 @@ node* build_tree(vector<int> vec, int i,int n){
         q.pop();
 
         
+        if (i<n){
+            temp -> lc = new node(vec[i]);
+            q.push(temp->lc);
+        }
+        i++;
+
+        if (i<n){
+            temp -> rc = new node(vec[i]);
+            q.push(temp->rc);
+        }
+        i++;
+
     }
+    return root;
+}
+
+bool is_BST(node* node,int min, int max){
+    if (node == NULL) return 1;
+    if (node->data < min || node->data > max) return 0;
+    return is_BST(node->lc,min,node->data) && is_BST(node->rc,node->data,max); 
+}
+
+int main(){
+    int n; cin >> n ;vector<int> vec;int val;
+
+    while(n){
+        cin >> val;
+        vec.push_back(val);
+        n--;
+    }
+
+    node* root = build_tree(vec,1,vec.size());
+
+    if (is_BST(root,INT_MIN,INT_MAX)) cout << root->data <<" YES";
+    else cout << root->data << " NO";
 }
