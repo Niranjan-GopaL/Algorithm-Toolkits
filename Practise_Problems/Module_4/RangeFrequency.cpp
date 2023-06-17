a8d5fb7f59ef34ebac3acb259a2daa673b11d9be
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 03:41:49 +0530

    rage freq complete in OOP

--- Practise_Problems/Module_4/RangeFrequency.cpp
+++ Practise_Problems/Module_4/RangeFrequency.cpp
@@ -28,9 +28,45 @@ class RangeFrequency{
             ST[i] = ST[2*i+1] + ST[2*i+2];
     }
 
+    int RangeQuery(int i,int ss,int se,int l,int r){
+        if (ss>=l and se <= r) return ST[i];
+        if (se<l or ss>r) return -1;
+
+        int mid = ss + (se-ss)/2;
+        int m1 = RangeQuery(2*i+1,ss,mid,l,r);
+        int m2 = RangeQuery(2*i+2,mid+1,se,l,r);
+
+        if (m1<0) return m2;
+        else if(m2<0) return m1;
+        else return m1+m2;
+    }
+
 };
 
 
 int main(){
-    return 0;
+    int n;cin >> n;int A[n];
+    for (int  i = 0; i < n; i++)
+        cin >> A[i];
+
+    int m;cin >> m;
+    int l,r,val;
+
+    RangeFrequency obj =  RangeFrequency(n,A);
+
+    int ans[m];int k = 0;
+    while(k<m){
+        cin >> l >> r >> val;
+        obj.making_ST(val);
+        if(!(l>=0 and r>=l and n>r)){
+            ans[k++] = -1;
+            continue;
+        }
+
+        int x = (int)((ceil)(log2(n)));
+        ans[k++] = obj.RangeQuery(0,0,pow(2,x)-1,l,r);
+    }
+
+    for(int i=0;i<m;i++)
+        cout << ans[i] << " ";
 }
