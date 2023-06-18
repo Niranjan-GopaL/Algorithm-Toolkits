312c30cb818a706757ef4cd6e3baad50cdecb040
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 15:53:39 +0530

    MY impl of BIT

---
+++ Examples and Theory/BIT/basics_my_impl_as_a_noob.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum_contained_at_idx(int idx,vector<int> vec){
+    int sum_ = 0;
+    while(idx){
+        sum_ = sum_ + vec[idx];
+        idx -= idx & -idx;
+    }
+    return sum_;
+}
+
+vector<int> update(int idx, int increase,vector<int> BIT){
+    while (idx < BIT.size())
+    {
+        BIT[idx] += increase;
+        idx += idx & -idx;
+    }
+    return BIT;
+}
+
+int main(){
+
+    int n; cin >> n ;vector<int> vec;int val;
+    while(n){
+        cin >> val;
+        vec.push_back(val);
+        n--;
+    }
+
+    // prints the inputed vector
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << endl;
+
+    // Inserting into the vector
+    // the working of this impl is pretty fked up
+    // turns out even if a function modifies vector it woun't be affected.
+    // so I had to write this complicated working shitty function.
+    // IT WORKS BUT IS TERRIBLE
+    vector<int> BIT(vec.size()+1,0);
+    for (int i=1; i < vec.size()+1; i++)        
+         BIT = update(i,vec[i-1],BIT);
+
+    // Prints the BIT
+    for (int i = 1; i < vec.size()+1; i++)
+        cout << BIT[i] << " ";
+
+}
