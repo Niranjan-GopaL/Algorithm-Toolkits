d026bb74bd7886225907fadcd3325f5eef2b1e38
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-22 21:13:45 +0530

    misc

--- benchmark/clipper.py
+++ benchmark/clipper.py
@@ -4,6 +4,19 @@ import time
 time.sleep(7)
 p.typewrite(
 '''
+
+
+visited=set()
+c=0
+
+for i in range(m):
+    for j in range(n):
+        if grid[i][j] and (i,j) not in visited:
+            c+=1
+            bfs(i,j)
+
+print(c)
+
 '''
 )
 p.typewrite(['enter'])
